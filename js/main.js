f57989cadc9dfee143352790ba9ae18ad778bc3a
maxmustermann987 <108799112+maxmustermann987@users.noreply.github.com>
2022-09-10 18:07:11 +0200

    10-09_4

--- js/main.js
+++ js/main.js
@@ -217,36 +217,11 @@ window.addEventListener("load", () => {
   //when i started working
   const startYear = 2005;
 
-  document.querySelector(".availability").innerText = getAvailability(currentMonth, currentFullYear);
+ 
   document.querySelector(".experience").innerText = getYearsExperience(startYear, currentFullYear);
 });
 
-function getAvailability(cMonth, cYear) {
-  const defaultText = "Wieder verfügbar ab ";
-  let availabilityDate;
 
-  switch (cMonth) {
-    case 0:
-    case 1:
-      availabilityDate = "März " + cYear;
-      break;
-    case 2:
-    case 3:
-    case 4:
-      availabilityDate = "Juni " + cYear;
-      break;
-    case 5:
-    case 6:
-    case 7:
-      availabilityDate = "September " + cYear;
-      break;
-    case 8:
-    case 9:
-    case 10:
-    case 11:
-      availabilityDate = "Januar " + (cYear + 1);
-      break;
-  }
 
   const infoText = defaultText + availabilityDate;
   // console.log(infoText);
